6df5415749cbcbc7d370f13f2705ab5268419a7c
Cryp70m4n <70487877+Cryp70m4n@users.noreply.github.com>
2022-09-26 20:30:26 +0000

    Add files via upload

---
+++ hellsing.h
@@ -0,0 +1,16 @@
+/* tagging */
+static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+/* fonts & size */
+static const char *fonts[]          = { "Iosevka:size=12" };
+static const char dmenufont[]       = "Iosevka:size=12";
+
+
+/* colors */
+static const char col_gray1[] = "#040404"; // deselected bg
+static const char col_gray2[] = "#3c4444"; // deselected border
+static const char col_gray3[] = "#7b7c7c"; // deselected text
+static const char col_gray4[] = "#c9cac9"; // selected text
+static const char col_gray5[] = "#c9cac9"; // selected border
+static const char col_cyan[] = "#040404"; // selected bg
+
